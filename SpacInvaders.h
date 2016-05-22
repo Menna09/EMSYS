d355c27e0cee0e36c9b062c278a84cb9cdef7a77
Menna09 <mennaezzat746@gmail.com>
2016-05-22 21:37:18 +0200

    Add files via upload

---
+++ SpacInvaders.h
@@ -0,0 +1,11 @@
+
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void Timer2_Init(unsigned long period);
+void Delay100ms(unsigned long count);
+void PortF_Init(void);// time delay in 0.1 seconds
+
+
+void shot (void);
+void Game_over(void);
+void Enemies (void);
